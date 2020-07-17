03e356ee7438ec387952239aa0e3d4f2176910fd
GKaszewski <gabrielkaszewski@gmail.com>
2020-07-17 18:56:26 +0200

    Added checking before creating new objects and added uploading json with data

--- routes/word.route.js
+++ routes/word.route.js
@@ -2,20 +2,56 @@ const routes = require('express').Router();
 const db = require('../database');
 const Word = require('../models/word.model');
 
-routes.get('/', async(req, res)=>{
+routes.get('/', async (req, res) => {
     await db.sync();
     let data = await Word.findAll();
     res.send(data);
 });
 
-routes.post('/new', async(req, res)=>{
+routes.post('/new', async (req, res) => {
     await db.sync();
     let data = req.body;
-    let newWord = await Word.create({
-        Spanish : data['Spanish'],
-        English : data['English'],
+    let newWord = await Word.findOrCreate({
+        where: {
+            Spanish: data['Spanish']
+        },
+        defaults: {
+            Spanish: data['Spanish'],
+            English: data['English'],
+        }
+    }).then(result => {
+        var created = result[1];
+
+        if (!created) {
+            return res.send('Word already exists in database!');
+        }
     });
-    res.send(newWord.toJSON());
+    return res.send(newWord.toJSON());
+});
+
+routes.post('/new/json', async (req, res) => {
+    await db.sync();
+    let data = req.body;
+    let words = [];
+    for (const word of data) {
+        await Word.findOrCreate({
+            where: {
+                Spanish: word['Spanish']
+            },
+            defaults: {
+                Spanish: word['Spanish'],
+                English: word['English'],
+            }
+        }).then(result => {
+            var actualWord = result[0];
+            var created = result[1];
+
+            if (created) {
+                words.push(actualWord);
+            }
+        });
+    }
+    return res.send(`Added: ${words.length} new words!`);
 });
 
 module.exports = routes;
