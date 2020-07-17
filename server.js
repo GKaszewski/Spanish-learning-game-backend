6cd7aaef16f5ac03820789be98aa9a2fc4574a92
GKaszewski <gabrielkaszewski@gmail.com>
2020-07-17 19:12:58 +0200

    Added user route

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require( "express" );
 const app = express();
+const path = require('path');
 var env = process.env.NODE_ENV || 'dev';
 const config = require('./environment')[env];
 const db = require('./database');
 
 //Routes
 const wordRoutes = require('./routes/word.route');
+const userRoutes = require('./routes/user.route');
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('static'));
 app.use('/api/word', wordRoutes);
+app.use('/', userRoutes);
 app.get("/", async(req, res)=> {
     await db.authenticate();
-    res.send('Hello, this is spanish learning game backend!');
+    res.sendFile(path.join(__dirname+'/static/index.html'));
 });
 
 app.listen(config.server.port);
