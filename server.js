f19558f0c929d4a1864fab0618e7c81e2c836e03
GKaszewski <gabrielkaszewski@gmail.com>
2020-07-17 18:14:30 +0200

    Added word routes to the server

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ var env = process.env.NODE_ENV || 'dev';
 const config = require('./environment')[env];
 const db = require('./database');
 
+//Routes
+const wordRoutes = require('./routes/word.route');
+
 app.use(express.json());
+app.use('api/word', wordRoutes);
 app.get("/", async(req, res)=> {
     await db.authenticate();
     res.send('Hello, this is spanish learning game backend!');
